1e479e6a2739bfdd59e3b4c82f54dd23b8ca33b6
eshapatel11 <eshapatel1@live.co.uk>
2025-05-13 19:10:04 +0100

    Use env variables for Cloudinary credentials

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -44,8 +44,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ecom',
     'widget_tweaks',
+    'cloudinary',
+    'cloudinary_storage',
 
 ]
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+  'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+  'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+  'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
