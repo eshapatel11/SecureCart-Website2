7a978fee63d8c21ad449497d896642557335e422
eshapatel11 <eshapatel1@live.co.uk>
2025-05-14 21:19:18 +0100

    Fix ALLOWED_HOSTS for local and Heroku deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['securecart-eshapatel-df12c7c8828c.herokuapp.com', 'securecart-eshapatel.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'securecart-eshapatel-df12c7c8828c.herokuapp.com']
 
 
 # Application definition
